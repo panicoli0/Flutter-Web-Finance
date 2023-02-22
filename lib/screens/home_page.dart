fc74407244e45f5ce5ad293c8bfd94d20a25b5ed
Pablo Ariel Nicoli <pablonicoli21@gmail.com>
2023-02-22 14:43:45 +0400

    new home page

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,101 @@
+import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
+import 'package:finance_web/widget/explore_drawer.dart';
+import 'package:finance_web/widget/responsive.dart';
+import 'package:finance_web/widget/top_bar_contents.dart';
+import 'package:finance_web/widget/web_scrollbar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  static const String route = '/';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late ScrollController _scrollController;
+  double _scrollPosition = 0;
+  double _opacity = 0;
+
+  _scrollListener() {
+    setState(() {
+      _scrollPosition = _scrollController.position.pixels;
+    });
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    _opacity = _scrollPosition < screenSize.height * 0.40
+        ? _scrollPosition / (screenSize.height * 0.40)
+        : 1;
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      extendBodyBehindAppBar: true,
+      appBar: ResponsiveWidget.isSmallScreen(context)
+          ? AppBar(
+              backgroundColor:
+                  Theme.of(context).bottomAppBarColor.withOpacity(_opacity),
+              elevation: 0,
+              centerTitle: true,
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.brightness_6),
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {
+                    EasyDynamicTheme.of(context).changeTheme();
+                  },
+                ),
+              ],
+              title: Text(
+                'MYSQUAD',
+                style: TextStyle(
+                  color: Colors.blueGrey[100],
+                  fontSize: 20,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 3,
+                ),
+              ),
+            )
+          : PreferredSize(
+              preferredSize: Size(screenSize.width, 1000),
+              child: TopBarContents(_opacity),
+            ),
+      drawer: ExploreDrawer(),
+      body: WebScrollbar(
+        color: Colors.blueGrey,
+        backgroundColor: Colors.blueGrey.withOpacity(0.3),
+        width: 10,
+        heightFraction: 0.3,
+        controller: _scrollController,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          physics: ClampingScrollPhysics(),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  SizedBox(
+                    height: screenSize.height * 0.45,
+                    width: screenSize.width,
+                  ),
+                ],
+              ),
+              SizedBox(height: screenSize.height / 10),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
